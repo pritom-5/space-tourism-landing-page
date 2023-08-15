4e8134007a62abdfcd039bd646edd045bd932b51
pritom <pritom@pop-os.localdomain>
2023-08-15 23:04:28 +0600

    test moon link

--- data.mjs
+++ data.mjs
@@ -10,8 +10,8 @@ const json_data = {
     {
       name: "Moon",
       images: {
-        png: "./assets/destination/image-moon.png",
-        webp: "./assets/destination/image-moon.webp",
+        png: "assets/destination/image-moon.png",
+        webp: "assets/destination/image-moon.webp",
       },
       description:
         "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
